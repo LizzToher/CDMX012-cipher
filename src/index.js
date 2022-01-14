3a32bef3d2e8718bd6181a402703c0aad77eca4b
Allison Tovar <alis.th94@gmail.com>
2022-01-14 11:16:06 -0600

    Añadir estructura al HTML, Agregar estilos con el CSS y guardar los inputs en variables para después darle funcionamiento al botón.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
 
+//const displaceNumber = document.querySelector('.number');
+const encodeBtn = document.getElementById('encode-btn');
+const decodeBtn = document.getElementById('decode-btn');
+let inputText = document.getElementById('input-text');
+let outputText = document.getElementById('output-text');
+
+encodeBtn.addEventListener('click', encode);
+decodeBtn.addEventListener('click', decode);
+
+function encode() {  
+   let encodedText = inputText.value;
+   outputText.innerHTML = encodedText;
+}
+
+function decode() {  
+    let encodedText = inputText.value;
+    outputText.innerHTML = encodedText;
+ }
+/*
 console.log(cipher);
+*/
