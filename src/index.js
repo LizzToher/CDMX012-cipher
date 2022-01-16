7a6b51e7ea1cad2ad7d40db0bff3f4bae28eb9aa
Allison Tovar <alis.th94@gmail.com>
2022-01-15 18:43:12 -0600

    Se añade variable que guarda la información de las entradas de texto y se agregan alertas para indicar al usuario que debe ingresar un número o texto para poder usar la página.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
-//import cipher from './cipher.js';
+import cipher from './cipher.js';
 
-//const displaceNumber = document.querySelector('.number');
-const encodeBtn = document.getElementById('encode-btn');
-const decodeBtn = document.getElementById('decode-btn');
-let inputText = document.getElementById('input-text');
+//Definiendo variables
+let displaceNumber = document.getElementById('offset');
+let encodeBtn = document.getElementById('encode-btn');
+let decodeBtn = document.getElementById('decode-btn');
+let string = document.getElementById('input-text');
 let outputText = document.getElementById('output-text');
 
+//Definiendo eventos
 encodeBtn.addEventListener('click', encode);
 decodeBtn.addEventListener('click', decode);
 
+//Añadiendo funcionalidad al botón de cifrar
 function encode() {  
-   let encodedText = inputText.value;
-   outputText.innerHTML = encodedText;
+   let encodedText = string.value;
+   let offset = displaceNumber.value;
+
+   if(offset == '') {
+      alert('Necesitas ingresar un número.');
+   }
+   else if(encodedText == '') {
+      alert('Necesitas ingresar tu mensaje.');
+   }
+   else {
+      outputText.innerHTML = encodedText;
+   }
 }
 
+//Añadiendo funcionalidad al botón de descifrar
 function decode() {  
-    let encodedText = inputText.value;
+    let encodedText = string.value;
+    let offset = displaceNumber.value;
+
+    if(offset == '') {
+      alert('Necesitas ingresar un número.');
+   }
+   else if(encodedText == '') {
+      alert('Necesitas ingresar tu mensaje.');
+   }
+   else {
     outputText.innerHTML = encodedText;
+   }
  }
-/*
+
 console.log(cipher);
-*/
